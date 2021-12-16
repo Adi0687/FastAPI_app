da4352d347646dd47f3770cabf325ece09fbcae3
Aadil Feroze <aadil.ahmed@live.iium.edu.my>
2021-12-16 21:08:48 +0300

    push to Heroku

--- app/main.py
+++ app/main.py
@@ -30,4 +30,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to my API!"}
+    return {"message": "Welcome to my API! This is part of my CI/CD Pipeline!!"}
